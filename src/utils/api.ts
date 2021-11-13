00524427423db00ec9f45907b7434c8a76b5dc94
Akash Krishnan <akashkrishnan98@gmail.com>
2021-11-13 21:55:20 +0530

    DEV: Test eslint

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,5 @@
 import { IUserApiResponse } from "../../pages/api/user";
+import ENV from "../constants/env";
 
 export const fetchUser = async (): Promise<IUserApiResponse> => {
   const res = await fetch(`/api/user`);
