54d9f2d450743c12030c16170a4ec333a4d1adb4
Akash Krishnan <akashkrishnan98@gmail.com>
2021-11-13 21:51:24 +0530

    DEV: Cleaned some unused vars

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,4 @@
 import { IUserApiResponse } from "../../pages/api/user";
-import ENV from "../constants/env";
 
 export const fetchUser = async (): Promise<IUserApiResponse> => {
   const res = await fetch(`/api/user`);
