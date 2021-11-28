61489f40cf826b224e679875f910281c0657a3b7
Akash Krishnan <akashkrishnan98@gmail.com>
2021-11-28 18:33:06 +0530

    Added secure cookie

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -66,6 +66,7 @@ const refreshAccessToken = async (token: JWT): Promise<JWT> => {
 
 export default NextAuth({
   secret: ENV.NEXTAUTH_SECRET,
+  useSecureCookies: !(process.env.DEPLOY_ENV === "dev"),
   providers: [
     CognitoProvider({
       clientId: ENV.COGNITO_CLIENT_ID,
