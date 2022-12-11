930a0b3e03ab94e7534fba66aad1b075b87ca436
Deemanth Poonacha <89336941+DeemanthPoonacha@users.noreply.github.com>
2022-12-11 13:12:44 +0530

    Update PostActionBar.tsx

--- src/containers/Posts/PostEditor/PostActionBar/PostActionBar.tsx
+++ src/containers/Posts/PostEditor/PostActionBar/PostActionBar.tsx
@@ -5,7 +5,6 @@ import PhotoCameraBackIcon from "@mui/icons-material/PhotoCameraBack";
 import CustomButton from "../../../../components/CustomButton";
 import { LABELS } from "../../../../constants/labels";
 import { useTextEditorContext } from "../../../../contexts/TextEditorContext";
-import { useEffect } from "react";
 
 interface IPostActionBarProps {}
 
@@ -34,7 +33,6 @@ const PostActionBar: React.FC<IPostActionBarProps> = ({}) => {
             <Grid item></Grid>
           </Grid>
         </Grid>
-        {isEditorEmpty.toString()}
         <Grid item>
           <CustomButton onClick={handlePostCreate} disabled={isEditorEmpty}>
             {LABELS.SUBMIT_POST}
